9a7e7d4751f9fe811560f1ab3f44a93a3276d60e
Jason Caldwell <jdcald13@gmail.com>
2017-11-04 15:50:54 -0600

    Commented out test cases

--- tests/com/skilldistillery/presidents/data/tests/PresidentDAOImplTest.java
+++ tests/com/skilldistillery/presidents/data/tests/PresidentDAOImplTest.java
@@ -1,24 +1,24 @@
-package com.skilldistillery.presidents.data.tests;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class PresidentDAOImplTest {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//package com.skilldistillery.presidents.data.tests;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//class PresidentDAOImplTest {
+//
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+//	@AfterEach
+//	void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
